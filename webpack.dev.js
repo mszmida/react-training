8543a9b6d20ae270206a78f8bf5b4e509c759bad
Maciej Szmida <maciej_szmida@epam.com>
2018-05-07 00:09:29 +0200

    webpack configuration splitted into separate files

---
+++ webpack.dev.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+const webpack = require("webpack");
+
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "source-map",
+    devServer: {
+        contentBase: path.resolve(__dirname, "build"),
+        compress: true,
+        hot: true
+    },
+    output: {
+        filename: "[name].bundle.js",
+        chunkFilename: "[name].chunk.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader/url" },
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: '[path][name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+});
