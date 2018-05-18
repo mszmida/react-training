1af32abd2e7162bd4bfad816da32862ebeca1300
Maciej Szmida <maciej_szmida@epam.com>
2018-05-19 01:37:46 +0200

    code review part 2 - removed initial components

--- src/components/initial/FourthWay.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const FourthWayComponent = () => {
-    return <p className="component">This component has been created as a functional component.</p>;
-};
-
-export default FourthWayComponent;
--- src/components/initial/SecondWay.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export class SecondWayComponent extends React.Component {
-    render() {
-        return(
-            <p className="component">This component has been created by extending of <code>React.Component</code> class.</p>
-        )
-    }
-}
