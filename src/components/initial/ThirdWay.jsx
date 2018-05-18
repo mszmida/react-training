a683d1bf1638543d2daf44ec8097c43ad2e7c674
Maciej Szmida <maciej_szmida@epam.com>
2018-05-19 01:37:46 +0200

    code review part 2 - removed initial components

--- src/components/initial/ThirdWay.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export class ThirdWayComponent extends React.PureComponent {
-    render() {
-        return (
-            <p className="component">This component has been created by extending of <code>React.PureComponent</code> class.</p>
-        )
-    }
-}
