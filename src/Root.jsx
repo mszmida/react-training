8d51cc2781db1b74943fda7b3bf016374aee4153
Maciej Szmida <maciej_szmida@epam.com>
2018-05-23 13:39:48 +0200

    error boundary changes

--- src/Root.jsx
+++ src/Root.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Logo from './common/components/logo/Logo';
 import Button from './common/components/Button';
 
-import ErrorBoundary from './components/error-boundary/ErrorBoundary';
 import Header from './components/header/Header';
 import SearchContainer from './containers/SearchContainer';
 import MovieDetailsBox from './components/movie-details-box/MovieDetailsBox';
@@ -59,35 +58,33 @@ export default class Root extends React.Component {
         const genre = movieDetails && movieDetails.genres && movieDetails.genres[0];
 
         return (
-            <ErrorBoundary>
-                <div className="page-wrapper">
-                    <Header
-                        topLeft={ <Logo /> }
-                        topRight={ movieDetails &&
-                            <Button
-                                className="button search-return-button"
-                                content="Search"
-                                onClickHandler={ this.handleSearchReturn } /> } >
+            <div className="page-wrapper">
+                <Header
+                    topLeft={ <Logo /> }
+                    topRight={ movieDetails &&
+                        <Button
+                            className="button search-return-button"
+                            content="Search"
+                            onClickHandler={ this.handleSearchReturn } /> } >
 
-                        { !movieDetails &&
-                            <SearchContainer
-                                onSearchResultsFetch={ this.handleSearchResults } />
-                        }
+                    { !movieDetails &&
+                        <SearchContainer
+                            onSearchResultsFetch={ this.handleSearchResults } />
+                    }
 
-                        { movieDetails &&
-                            <MovieDetailsBox
-                                { ...movieDetails }
-                                genre={ genre } />
-                        }
-                    </Header>
+                    { movieDetails &&
+                        <MovieDetailsBox
+                            { ...movieDetails }
+                            genre={ genre } />
+                    }
+                </Header>
 
-                    <MoviesList movies={ movies } onMovieClick={ this.handleMovieClick } />
+                <MoviesList movies={ movies } onMovieClick={ this.handleMovieClick } />
 
-                    <Footer>
-                        <Logo />
-                    </Footer>
-                </div>
-            </ErrorBoundary>
+                <Footer>
+                    <Logo />
+                </Footer>
+            </div>
         );
     }
 }
